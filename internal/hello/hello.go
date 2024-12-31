e81e222be2f4e17bbb85fa6906470327f8ab536e
Yu Chun Tsao <tsao84672776@gmail.com>
2024-12-31 10:54:45 +0800

    test

--- internal/hello/hello.go
+++ internal/hello/hello.go
@@ -8,7 +8,7 @@ package hello
 #cgo linux CFLAGS: -I../../pkg/test-rust/lib
 #cgo darwin LDFLAGS: ./pkg/test-rust/lib/hello/target/release/libhello.dylib -ldl
 #cgo darwin CFLAGS: -I../../pkg/test-rust/lib
-#cgo windows LDFLAGS: ./pkg/test-rust/lib/hello/target/release/hello.dll
+#cgo windows LDFLAGS: ./pkg/test-rust/lib/hello/target/release/hello.lib
 #cgo windows CFLAGS: -I../../pkg/test-rust/lib
 #include "hello.h"
 #include <stdlib.h>
