2328fe141686a47bd8928f70867da7fa6745f82d
Yu Chun Tsao <tsao84672776@gmail.com>
2024-12-31 11:32:59 +0800

    test

--- internal/hello/hello.go
+++ internal/hello/hello.go
@@ -3,13 +3,14 @@ package hello
 // NOTE: There should be NO space between the comments and the `import "C"` line.
 // The -ldl is sometimes necessary to fix linker errors about `dlsym`.
 
+#cgo LDFLAGS: -L${SRCDIR}/../target/release -lbandersnatch_vrfs_ffi  -Wl,-rpath,${SRCDIR}/../target/release
 /*
 #cgo linux LDFLAGS: ./pkg/test-rust/lib/hello/target/release/libhello.so -ldl
 #cgo linux CFLAGS: -I../../pkg/test-rust/lib
 #cgo darwin LDFLAGS: ./pkg/test-rust/lib/hello/target/release/libhello.dylib -ldl
 #cgo darwin CFLAGS: -I../../pkg/test-rust/lib
-#cgo windows LDFLAGS: -L./pkg/test-rust/lib/hello/target/release -lhello
-#cgo windows CFLAGS: -I../../pkg/test-rust/lib
+#cgo windows LDFLAGS: -L${SRCDIR}/pkg/test-rust/lib/hello/target/release -lhello
+#cgo windows CFLAGS: -I${SRCDIR}/pkg/test-rust/lib
 #include "hello.h"
 #include <stdlib.h>
 */
